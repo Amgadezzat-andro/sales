0d99da03f6da95dc89d5575db7dbed5aeca7d11a
Amgad Ezzat <megostar59@gmail.com>
2026-01-11 16:34:07 +0200

    setup admin layout

---
+++ resources/views/test.blade.php
@@ -0,0 +1,19 @@
+
+@extends('layouts.admin')
+
+@section('title')
+test TITLE
+@endsection
+
+@section('contentheader')
+test TITLE HEAdER
+@endsection
+
+@section('content')
+test hereer
+@endsection
+
+@section('content')
+test hereer
+@endsection
+
